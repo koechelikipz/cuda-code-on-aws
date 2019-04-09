e7dfec4974ffd0db8ae04eddac824d6116bcf4ed
sarat <saratlingamarla@gmail.com>
2019-04-08 20:14:21 -0400

    Update main.cu

--- main.cu
+++ main.cu
@@ -23,21 +23,35 @@ using namespace std;
 
 // Function Definitions
 
+//
+// Arguments    : const char *
+
+// Return Type  : emxArray_char_T *
+//
 static emxArray_char_T *argInit_1xUnbounded_char_T(const char * file)
 {
   emxArray_char_T *result;
   int32_T nameSize;
-  // Set the size of the array.
-  // Change this size to the value that the application requires.
+  
+  // Set the size of the array based on input filename string
   nameSize = strlen(file)+1;
-  cout << nameSize << flush;
+  
+  // initialize an emxArray_char_T array
   result = emxCreate_char_T(1, nameSize);
-
+  
+  // copy input filename string to emxArray_char_T array
   strcpy(result->data,file);
 
   return result;
 }
 
+
+//
+// Arguments    : emxArray_char_T *
+//                char_T *
+
+// Return Type  : void
+//
 static void main_alexnet_predict(emxArray_char_T *filename, char_T* out_data)
 {
   int32_T out_size[2];
@@ -46,6 +60,7 @@ static void main_alexnet_predict(emxArray_char_T *filename, char_T* out_data)
   alexnet_predict(filename, out_data, out_size);
   emxDestroyArray_char_T(filename);
 }
+
 
 //
 // Arguments    : int32_T argc
@@ -61,11 +76,12 @@ int32_T main(int32_T argc, const char * argv[])
   // Initialize the application.
   alexnet_predict_initialize();
 
-  // convert string filename input to 'emxArray_char_T' type  
+  // convert filename input from string to 'emxArray_char_T' type  
   file = argInit_1xUnbounded_char_T(argv[1]);
 
   // Invoke the entry-point functions.
   main_alexnet_predict(file,result);
+  // print classification output to standard output
   cout << result;
 
   // Terminate the application.
